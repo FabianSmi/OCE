54ad57857a8b53221e9bdade9055719c34cacdce
FabianSmi <darkscorpion1@gmx.de>
2025-04-25 12:57:18 +0200

    created UI Manager Class

---
+++ Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class UI_Manager : MonoBehaviour
+{
+    static public UI_Manager Instance;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void SetHpBar()
+    {
+
+    }
+}
